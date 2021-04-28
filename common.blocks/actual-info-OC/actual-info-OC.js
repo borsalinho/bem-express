68049b215c0a4afbdfad35514648848566453ed8
sanadal-office <dfvgbhcvbn@mail.ru>
2021-04-28 16:35:29 +0900

    допилил слайдер актуальной инфы

--- common.blocks/actual-info-OC/actual-info-OC.js
+++ common.blocks/actual-info-OC/actual-info-OC.js
@@ -4,8 +4,32 @@ provide(bemDom.declBlock(this.name, {
     onSetMod: {
         js: {
             inited: function() {
-                $(document).ready(function(){   
-                    $('.actual-info-OC__slider').slick({
+                $(document).ready(function(){
+
+                    var $slider = $('.actual-info-OC__slider');
+
+                    if ($slider.length) {
+                    var currentSlide;
+                    var slidesCount;
+                    var sliderCounter = document.createElement('div');
+                    sliderCounter.classList.add('slider__counter');
+                    
+                    var updateSliderCounter = function(slick, currentIndex) {
+                        currentSlide = slick.slickCurrentSlide() + 1;
+                        slidesCount = slick.slideCount;
+                        $(sliderCounter).text(currentSlide + '/' +slidesCount)
+                    };
+
+                    $slider.on('init', function(event, slick) {
+                        $slider.append(sliderCounter);
+                        updateSliderCounter(slick);
+                    });
+
+                    $slider.on('afterChange', function(event, slick, currentSlide) {
+                        updateSliderCounter(slick, currentSlide);
+                    });
+
+                    $slider.slick({
                         lazyLoad: 'ondemand',
                         dots: false,
                         arrows: true,
@@ -14,8 +38,19 @@ provide(bemDom.declBlock(this.name, {
                         slidesToShow: 1,
                         slidesToScroll: 1,
                     });
+                    }
+                       
+                    // $('.actual-info-OC__slider').slick({
+                    //     lazyLoad: 'ondemand',
+                    //     dots: false,
+                    //     arrows: true,
+                    //     infinite: true,
+                    //     speed: 300,
+                    //     slidesToShow: 1,
+                    //     slidesToScroll: 1,
+                    // });
                     $('.actual-info-OC__skeleton').css({'display' : 'none'});
-                    $('.actual-info-OC__slider').show();
+                    $slider.show();
                 });
             }
         }
