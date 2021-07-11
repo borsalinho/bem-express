c7a219d9f7390015428cf7e2ef1a373964f5ee0c
borsalinho <dfvgbhcvbn0@gmail.com>
2021-07-11 16:05:19 +0900

    +ecoclick

--- common.blocks/main-EC/main-EC.bemtree.js
+++ common.blocks/main-EC/main-EC.bemtree.js
@@ -5,10 +5,31 @@ block("main-EC")({
             elem:'a',
             tag:'section',
             content:{
-                cls:'conteiner',
-                content:{
-
-                }
+                cls:'container',
+                content:[
+                    {
+                        elem:'a-title',
+                        tag:'h1',
+                        content:'Сохраним природу нашей республики'
+                    },
+                    {
+                        elem:'a-title-2',
+                        tag:'h2',
+                        content:'Сервис по мониторингу экологических проблем Республики Саха (Якутии)'
+                    },
+                    {
+                        elem:'a-text',
+                        tag:'p',
+                        content:'Перейти на "Экологический паспорт РС (Я)", портал министерства экологии, природопользования и лесного хозяйства Республики Саха (Якутия)'
+                    },
+                    {
+                        tag:'a',
+                        attrs:{
+                            href:'http://prirodayakutia.sakha.gov.ru/'
+                        },
+                        content:'http://prirodayakutia.sakha.gov.ru/'
+                    }
+                ]
             }
         }
     ],
