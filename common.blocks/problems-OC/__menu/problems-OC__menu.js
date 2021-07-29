a06fe1d25a67f979cc28b8fbecf7964d738187f6
borsalinho <dfvgbhcvbn0@gmail.com>
2021-07-29 20:24:49 +0900

    в /problems по прозьбе Баира поменял стиль смены тэгов. было по клику, стало при наведении

--- common.blocks/problems-OC/__menu/problems-OC__menu.js
+++ common.blocks/problems-OC/__menu/problems-OC__menu.js
@@ -7,22 +7,44 @@ modules.define('problems-OC__menu', ['i-bem-dom'], function(provide, bemDom) {
                     $('.problems-OC__menu-item:first .problems-OC__menu-item-img').addClass( "problems-OC__menu-item-img_rotate");
                     $('.problems-OC__menu-item:first span').css('border-bottom','2px solid rgb(3, 90, 166)').css('padding-bottom','2px');
                         
-                    $(".problems-OC__menu-item").on("click", function () {
+                    // $(".problems-OC__menu-item").on("click", function () {
+                    //     var index = $(".problems-OC__menu-item").index(this);
+                    //     var indexImg = $(".problems-OC__menu-item .problems-OC__menu-item-img");
+                    //     var indexSpan = $(".problems-OC__menu-item span")
+                        
+                    //     var $tags = $(".problems-OC__tags-item");
+                    //     if ($tags.eq(index).is(":visible")) {
+                    //         $tags.eq(index).hide("fast");
+                    //         indexImg.eq(index).removeClass( "problems-OC__menu-item-img_rotate");
+                    //         indexSpan.eq(index).css('border-bottom','none');
+                    //     } else {
+                    //         $tags.hide("fast");
+                    //         indexImg.removeClass( "problems-OC__menu-item-img_rotate" );
+                    //         indexSpan.css('border-bottom','none');
+
+                    //         $tags.eq(index).show("fast");
+                    //         indexImg.eq(index).addClass( "problems-OC__menu-item-img_rotate" );
+                    //         indexSpan.eq(index).css('border-bottom','2px solid rgb(3, 90, 166)').css('padding-bottom','2px');;
+                    //     }
+                    // });
+
+                    
+                    $(".problems-OC__menu-item").hover( function () {
                         var index = $(".problems-OC__menu-item").index(this);
                         var indexImg = $(".problems-OC__menu-item .problems-OC__menu-item-img");
                         var indexSpan = $(".problems-OC__menu-item span")
-                        
                         var $tags = $(".problems-OC__tags-item");
-                        if ($tags.eq(index).is(":visible")) {
-                            $tags.eq(index).hide("fast");
-                            indexImg.eq(index).removeClass( "problems-OC__menu-item-img_rotate");
-                            indexSpan.eq(index).css('border-bottom','none');
-                        } else {
-                            $tags.hide("fast");
-                            indexImg.removeClass( "problems-OC__menu-item-img_rotate" );
-                            indexSpan.css('border-bottom','none');
 
-                            $tags.eq(index).show("fast");
+
+                        if ($tags.eq(index).hasClass("problems-OC__tags-item_visible_true")) {
+
+                        }  else {
+                            // удаляем ото всех
+                            $($tags).removeClass( "problems-OC__tags-item_visible_true");indexImg.removeClass( "problems-OC__menu-item-img_rotate");
+                            indexSpan.css('border-bottom','none');
+                            
+                            // и добавляем только на наведенный
+                            $($tags.eq(index)).addClass( "problems-OC__tags-item_visible_true");
                             indexImg.eq(index).addClass( "problems-OC__menu-item-img_rotate" );
                             indexSpan.eq(index).css('border-bottom','2px solid rgb(3, 90, 166)').css('padding-bottom','2px');;
                         }
